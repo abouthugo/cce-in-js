e33a607d49b24a532c2fff7c9c1352feafb0436e
abouthugo <hugo@perdomo.dev>
2021-02-08 19:15:18 -0500

    adding iterative and recursive solutions to reverse list algo

--- src/2/leetcode/ReverseLinkedList.ts
+++ src/2/leetcode/ReverseLinkedList.ts
@@ -31,6 +31,37 @@ function reverseList(head: ListNode) {
   return head;
 }
 
+/**
+ * The iterative solution in leet code
+ * @param head
+ */
+function reverseList2(head: ListNode) {
+  let prev = null;
+  let current = head;
+  while (current) {
+    const temp = current.next;
+    current.next = prev;
+    prev = current;
+    current = temp;
+  }
+  return prev;
+}
+
+/**
+ * The recursive solution from leet code
+ * @param head
+ */
+function reverseListRecursive(head: ListNode): ListNode {
+  if (head === null || head.next === null) return head;
+  const endOfList = reverseListRecursive(head.next);
+  // [1] -> [2]
+  head.next.next = head;
+  // [2] -> [1] -> [2] (-> [1]..... recursive forever)
+  head.next = null;
+  // [2] -> 1 -> null
+  return endOfList;
+}
+
 interface ListNode {
   val: any;
   next: any;
